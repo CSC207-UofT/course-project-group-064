bdc3f25be891d4628678b3009ef903b385521ff9
Xuerong Zhou <snow.zhou@mail.utoronto.ca>
2021-11-29 14:09:53 -0500

    Some updates in ChessGameApplication

--- src/main/java/com/playchessgame/chessgame/ChessGameApplication.java
+++ src/main/java/com/playchessgame/chessgame/ChessGameApplication.java
@@ -3,12 +3,20 @@ package com.playchessgame.chessgame;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class ChessGameApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ChessGameApplication.class, args);
+
+//        SpringApplication.run(ChessGameApplication.class, args);
+//        System.setProperty("java.awt.headless", "false"); //Disables headless
+
+        SpringApplicationBuilder builder = new SpringApplicationBuilder(ChessGameApplication.class);
+        builder.headless(false);
+        builder.run(args);
+
     }
 
 }
